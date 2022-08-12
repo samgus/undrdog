e557f2fc32290a56b07989081403f363c3283d58
Sam Gustafsson <samgus93@gmail.com>
2022-08-12 15:45:30 -0400

    updated nav bar

--- src/App.js
+++ src/App.js
@@ -1,84 +1,27 @@
 import './App.css';
-import { FaStar } from 'react-icons/fa';
-import { useState } from 'react';
-import NavBar from './components/nav-bar/nav-bar.component';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const colors = {
-  orange: "#FFBA5A",
-  grey: "#a9a9a9"
-}
+import NavBar from './components/nav-bar/nav-bar.component';
+import Home from './pages/home';
+import About from './pages/about';
+import Services from './pages/services';
+import Contact from './pages/contact';
+import Signup from './pages/signup';
 
 function App() {
-  const stars = Array(5).fill(0);
-  const [currentValue, setCurrentValue] = useState(0);
-  const [hoverValue, setHoverValue] = useState(undefined);
-
-  const handleClick = value => {
-    setCurrentValue(value)
-  };
-
-  const handleMouseOver = value => {
-    setHoverValue(value)
-  };
-
-  const handleMouseLeave = () => {
-    setHoverValue(undefined)
-  }
   return (
     <Router>
-    <div style={styles.container}>
       <NavBar />
-      <h2>Rate your Restaurant</h2>
-      <div style={styles.stars}>
-        {stars.map((_,index) => {
-          return (
-            <FaStar 
-              key={index}
-              size={24}
-              style={{
-                marginRight: 10,
-                cursor: "pointer",
-              }}
-              color={(hoverValue || currentValue) > index ? colors.orange: colors.grey}
-              onClick={() => handleClick(index + 1)}
-              onMouseOver={() => handleMouseOver(index + 1)}
-              onMouseLeave={handleMouseLeave}
-            />
-          )
-        })}
-      </div>
-      <textarea 
-        placeholder="What's your feedback"
-        style={styles.textarea}
-      />
-      <button style={styles.button}>Submit</button>
-    </div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/services' element={<Services/>} />
+        <Route path='/contact-us' element={<Contact/>} />
+        <Route path='/sign-up' element={<Signup/>} />
+      </Routes>
     </Router>
   );
 };
-
-const styles = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  textarea: {
-    border: "1px solid #a9a9a9",
-    borderRadius: 5,
-    width: 300,
-    margin: "20px 0",
-    minHeight: 100,
-    padding: 10
-  },
-  button: {
-    border: "1px solid #a9a9a9",
-    borderRadius: 5,
-    width: 300,
-    padding: 10
-  }
-}
 
 export default App;
 
--- src/components/nav-bar/nav-bar.component.jsx
+++ src/components/nav-bar/nav-bar.component.jsx
@@ -1,41 +1,43 @@
-import React, { Fragment } from 'react'
-import { 
-    Nav, 
-    NavLink, 
-    Bars, 
-    NavMenu, 
-    NavBtn, 
-    NavBtnLink 
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
 } from './nav-bar.styles';
 
 const NavBar = () => {
   return (
-    <Fragment>
+    <>
       <Nav>
         <NavLink to='/'>
-            <h1>Logo</h1>
+          <h1>U N D E R D O G</h1>
         </NavLink>
         <Bars />
         <NavMenu>
-            <NavLink to='/about' activeStyle>
-                About
-            </NavLink>
-            <NavLink to='/services' activeStyle>
-                Services
-            </NavLink>
-            <NavLink to='/contact-us' activeStyle>
-                Contact Us
-            </NavLink>
-            <NavLink to='/sign-up' activeStyle>
-                Sign Up
-            </NavLink>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/services' activeStyle>
+            Services
+          </NavLink>
+          <NavLink to='/contact-us' activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to='/sign-up' activeStyle>
+            Sign Up
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
         </NavMenu>
         <NavBtn>
-            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
         </NavBtn>
       </Nav>
-    </Fragment>
-  )
-}
+    </>
+  );
+};
 
 export default NavBar;
